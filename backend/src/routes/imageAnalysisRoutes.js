4676d3f816474ab297fd76aadf4c4e482ea7d376
feriel remadi <fremadi9@gmail.com>
2026-05-13 01:42:50 +0200

    axios add

--- backend/src/routes/imageAnalysisRoutes.js
+++ backend/src/routes/imageAnalysisRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const imageAnalysisController = require('../controllers/imageAnalysisController');
 const { authMiddleware } = require('../Middleware/Authmiddleware'); 
 console.log('authMiddleware =', authMiddleware);
